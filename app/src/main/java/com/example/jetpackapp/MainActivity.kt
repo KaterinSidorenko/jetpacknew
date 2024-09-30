dd1be3532eb2a4a9d85e79d49ce536093a06d0ca
KaterinSidorenko <skaterinap@gmail.com>
2024-09-30 20:18:36 +0300

    commitnew

--- app/src/main/java/com/example/jetpackapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpackapp/MainActivity.kt
@@ -14,10 +14,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,6 +28,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackapp.ui.theme.JetpackAppTheme
@@ -42,8 +45,6 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     PinCodeScreen()
-
-
                 }
             }
         }
@@ -54,6 +55,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun PinCodeScreen() {
     var inputText by remember { mutableStateOf("") }
+    var pinCode by remember { mutableStateOf("527131") }
+    var errorPin by remember { mutableStateOf(false) }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -61,33 +64,42 @@ fun PinCodeScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        TextBlock(inputText)
-        PinCodeBlock { buttonValue ->
+        TextBlock(inputText, errorPin)
+
+        PinCodeBlock({ buttonValue ->
             inputText += buttonValue // Обновляем inputText
-        }
+        },
+            onOkClick = {
+                errorPin = inputText != pinCode
+            })
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TextBlock(inputText: String) {
+fun TextBlock(inputText: String, errorPin: Boolean) {
     Column(
         modifier = Modifier
-            .padding(20.dp)
-    ) {
-        TextField(
-            modifier = Modifier.fillMaxWidth(),
-            enabled = false,
-            value = inputText,
-            onValueChange = {},
-            readOnly = true,
-            label = { Text(text = "Enter PIN")})
-    }
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
 
+    ) {
+        Text(
+            textAlign = TextAlign.Center,
+            text = inputText,
+            color = if (errorPin) Color.Red else Color.Black,
+            modifier = Modifier.fillMaxWidth(),
+
+            )
+    }
 }
 
 
 @Composable
-fun PinCodeBlock(onButtonClick: (String)->Unit) {
+fun PinCodeBlock(
+    onButtonClick: (String) -> Unit,
+    onOkClick: () -> Unit
+) {
 
     Row(
         modifier = Modifier
@@ -96,7 +108,7 @@ fun PinCodeBlock(onButtonClick: (String)->Unit) {
         horizontalArrangement = Arrangement.SpaceEvenly // Используйте SpaceEvenly для равномерного расстояния или просто SpaceBetween для уменьшения пустого пространства
     ) {
         CalculatorButton(onClick = { onButtonClick("1") }, text = "1")
-        CalculatorButton(onClick = {onButtonClick("2")}, text = "2")
+        CalculatorButton(onClick = { onButtonClick("2") }, text = "2")
         CalculatorButton(onClick = { onButtonClick("3") }, text = "3")
     }
     Row(
@@ -107,8 +119,8 @@ fun PinCodeBlock(onButtonClick: (String)->Unit) {
         horizontalArrangement = Arrangement.SpaceEvenly
     ) {
         CalculatorButton(onClick = { onButtonClick("4") }, text = "4")
-        CalculatorButton(onClick = {onButtonClick("5")}, text = "5")
-        CalculatorButton(onClick = {onButtonClick("6")}, text = "6")
+        CalculatorButton(onClick = { onButtonClick("5") }, text = "5")
+        CalculatorButton(onClick = { onButtonClick("6") }, text = "6")
     }
     Row(
         modifier = Modifier
@@ -116,9 +128,9 @@ fun PinCodeBlock(onButtonClick: (String)->Unit) {
             .fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceEvenly
     ) {
-        CalculatorButton(onClick = {onButtonClick("7")}, text = "7")
-        CalculatorButton(onClick = { onButtonClick("8")}, text = "8")
-        CalculatorButton(onClick = {onButtonClick("9") }, text = "9")
+        CalculatorButton(onClick = { onButtonClick("7") }, text = "7")
+        CalculatorButton(onClick = { onButtonClick("8") }, text = "8")
+        CalculatorButton(onClick = { onButtonClick("9") }, text = "9")
     }
     Row(
         modifier = Modifier
@@ -127,8 +139,8 @@ fun PinCodeBlock(onButtonClick: (String)->Unit) {
         horizontalArrangement = Arrangement.SpaceEvenly
     ) {
         CalculatorButton(onClick = { onButtonClick("") }, text = "<-")
-        CalculatorButton(onClick = {onButtonClick("0")}, text = "0")
-        CalculatorButton(onClick = { onButtonClick("")}, text = "OK")
+        CalculatorButton(onClick = { onButtonClick("0") }, text = "0")
+        CalculatorButton(onClick = { onOkClick() }, text = "OK")
 
 
     }
